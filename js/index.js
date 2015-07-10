6194455a25163bae46dffdd27041651859a15759
Juan Manuel <jmdonandueno@hotmail.com>
2015-07-09 23:19:08 -0300

    jQuery implementation
    creating async postback for text retieval + img fade in/out

--- js/index.js
+++ js/index.js
@@ -1,13 +1,33 @@
 $(document).ready(function(){
     resizeDiv();
+    $(".link").click(function() {
+        if(this.innerHTML === "PRINCIPAL")
+            __doPostBack();
+        write(this)
+    });
 });
 
 window.onresize = function(event) {
     resizeDiv();
 }
 
+function getAmazonURL (name) {
+    return "https://s3-us-west-2.amazonaws.com/content-angular-surgeon/" + name.toLowerCase();
+}
+
 function resizeDiv() {
     vph = $(window).height();
     $("body").css("height",vph);
-    $("article").css("height", vph -150);
+    $("article").css("height", vph - 150);
+}
+
+function write (e) {
+    $("img:not(disabled)").toggleClass("disabled");
+    $(e).toggleClass("disabled");
+    $.ajax(getAmazonURL(e.innerHTML)+".txt", 
+           {    
+                success: function(data){
+                    $("#article")[0].innerHTML = data;
+                }
+           });
 }
